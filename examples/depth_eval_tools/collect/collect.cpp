140a3b24221fc6ff7284e163d2d32d34a611231e
maniabarot <maria@scatter.nyc>
2021-03-23 19:19:48 +0100

    updated collect to capture from call connected devices before pausing between views

--- examples/depth_eval_tools/collect/collect.cpp
+++ examples/depth_eval_tools/collect/collect.cpp
@@ -81,7 +81,8 @@ int main(int argc, char **argv)
                "{gg|0.5| gray_gamma used to convert ir data to 8bit gray image}"
                "{gm|4000.0| gray_max used to convert ir data to 8bit gray image}"
                "{gp | 99.0 | percentile used to convert ir data to 8bit gray image}"
-               "{av|0| 0:dump mean images only, 1:dump all images, 2:dump all images and their mean}");
+               "{av|0| 0:dump mean images only, 1:dump all images, 2:dump all images and their mean}"
+               "{sf| 0 | startframe index}");
     // get input argument
 
     if (parser.has("help") || !parser.has("mode") || !parser.has("res") || !parser.has("out"))
@@ -112,6 +113,7 @@ int main(int argc, char **argv)
     float gray_max = parser.get<float>("gm");
     float gray_percentile = parser.get<float>("gp");
 
+    int startIndex = parser.get<int>("sf");
     uint32_t device_count = k4a::device::get_installed_count();
 
     if (device_count == 0)
@@ -119,178 +121,182 @@ int main(int argc, char **argv)
         std::cerr << "No K4A devices found\n";
         return 1;
     }
-
-    k4a::device device = k4a::device::open(K4A_DEVICE_DEFAULT);
-
-    k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
-    config.depth_mode = (k4a_depth_mode_t)mode;
-    config.color_format = K4A_IMAGE_FORMAT_COLOR_MJPG;
-    config.color_resolution = (k4a_color_resolution_t)res;
-    config.camera_fps = (k4a_fps_t)fps;
-
-    device.start_cameras(&config);
-
-    k4a::calibration calibration = device.get_calibration(config.depth_mode, config.color_resolution);
-
-    if (dump_calibration)
-    {
-        write_calibration_blob(device.get_raw_calibration(), output_dir, "calibration_blob");
-        write_opencv_calib(calibration.depth_camera_calibration, output_dir, "cal_depth_mode" + std::to_string(mode));
-        write_opencv_calib(calibration.color_camera_calibration, output_dir, "cal_color");
-    }
-
-    if (dump_xytable)
-    {
-        k4a::image xy_table = k4a::image::create(K4A_IMAGE_FORMAT_CUSTOM,
-                                                 calibration.depth_camera_calibration.resolution_width,
-                                                 calibration.depth_camera_calibration.resolution_height,
-                                                 calibration.depth_camera_calibration.resolution_width *
-                                                     (int)sizeof(k4a_float2_t));
-
-        create_xy_table(calibration, xy_table);
-        write_xy_table(xy_table, output_dir, "xy_table_mode" + std::to_string(mode));
-    }
-
-    k4a::capture capture;
-
-    std::chrono::milliseconds timeout_msec(1000);
-
     std::cout << "Capturing " << num_caps_per_view << " frames per view \n";
-    for (int view_idx = 0; view_idx < num_views; view_idx++)
+    num_views += startIndex;
+    for (int view_idx = startIndex; view_idx < num_views; view_idx++)
     {
-        std::cout << "Ready to capture next frame . Press Enter? \n";
-        // system("pause");
+        std::cout << "----------  Ready to capture next frame. ---------- Press Enter. " << std::endl;
         getchar();
-        cv::Mat mean_ir, mean_depth, mean_color[3];
-        int n_ir = 0, n_depth = 0, n_color = 0;
-
-        for (int cap_idx = 0; cap_idx < num_caps_per_view; cap_idx++)
+        for (uint32_t dev = 0; dev < device_count; ++dev)
         {
-            // Get a capture
-            if (!device.get_capture(&capture, timeout_msec))
-            {
-                std::cerr << "Timed out waiting for a capture\n";
-                break;
-            }
-            k4a::image depth_img;
-            k4a::image ir_img;
-            k4a::image color_img;
+            k4a::device device = k4a::device::open(dev);
+            std::string output_dev_dir = output_dir + "/" + std::to_string(dev);
+            k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+            config.depth_mode = (k4a_depth_mode_t)mode;
+            config.color_format = K4A_IMAGE_FORMAT_COLOR_MJPG;
+            config.color_resolution = (k4a_color_resolution_t)res;
+            config.camera_fps = (k4a_fps_t)fps;
 
-            if (dump_color)
+            device.start_cameras(&config);
+
+            k4a::calibration calibration = device.get_calibration(config.depth_mode, config.color_resolution);
+
+            if (dump_calibration)
             {
-                // get color image
-                color_img = capture.get_color_image();
-                if (color_img.is_valid())
+                write_calibration_blob(device.get_raw_calibration(), output_dev_dir, "calibration_blob");
+                write_opencv_calib(calibration.depth_camera_calibration,
+                                   output_dev_dir,
+                                   "cal_depth_mode" + std::to_string(mode));
+                write_opencv_calib(calibration.color_camera_calibration, output_dev_dir, "cal_color");
+            }
+
+            if (dump_xytable)
+            {
+                k4a::image xy_table = k4a::image::create(K4A_IMAGE_FORMAT_CUSTOM,
+                                                         calibration.depth_camera_calibration.resolution_width,
+                                                         calibration.depth_camera_calibration.resolution_height,
+                                                         calibration.depth_camera_calibration.resolution_width *
+                                                             (int)sizeof(k4a_float2_t));
+
+                create_xy_table(calibration, xy_table);
+                write_xy_table(xy_table, output_dev_dir, "xy_table_mode" + std::to_string(mode));
+            }
+
+            k4a::capture capture;
+
+            std::chrono::milliseconds timeout_msec(4000);
+
+            cv::Mat mean_ir, mean_depth, mean_color[3];
+            int n_ir = 0, n_depth = 0, n_color = 0;
+
+            for (int cap_idx = 0; cap_idx < num_caps_per_view; cap_idx++)
+            {
+                // Get a capture
+                if (!device.get_capture(&capture, timeout_msec))
                 {
-                    std::cout << " | Color res: " << color_img.get_height_pixels() << "x"
-                              << color_img.get_width_pixels();
-                    cv::Mat frame32[3];
-                    cv::Mat bands[3];
-                    cv::Mat M = color_to_opencv(color_img);
-                    cv::split(M, bands);
-                    for (int ch = 0; ch < 3; ch++)
+                    std::cerr << "Timed out waiting for a capture\n";
+                    break;
+                }
+                k4a::image depth_img;
+                k4a::image ir_img;
+                k4a::image color_img;
+
+                if (dump_color)
+                {
+                    // get color image
+                    color_img = capture.get_color_image();
+                    if (color_img.is_valid())
                     {
-                        bands[ch].convertTo(frame32[ch], CV_32F);
-                        if (n_color < 1)
-                            mean_color[ch] = frame32[ch].clone();
+                        std::cout << " | Color res: " << color_img.get_height_pixels() << "x"
+                                  << color_img.get_width_pixels();
+                        cv::Mat frame32[3];
+                        cv::Mat bands[3];
+                        cv::Mat M = color_to_opencv(color_img);
+                        cv::split(M, bands);
+                        for (int ch = 0; ch < 3; ch++)
+                        {
+                            bands[ch].convertTo(frame32[ch], CV_32F);
+                            if (n_color < 1)
+                                mean_color[ch] = frame32[ch].clone();
+                            else
+                                mean_color[ch] += frame32[ch].clone();
+                        }
+
+                        if (av > 0)
+                        {
+                            std::string tstr = std::to_string(color_img.get_device_timestamp().count());
+                            cv::imwrite(output_dev_dir + "/" + tstr + "-color.png", M);
+                        }
+                        n_color++;
+                    }
+                }
+
+                if (dump_depth)
+                {
+                    // get depth image
+                    depth_img = capture.get_depth_image();
+                    if (depth_img.is_valid())
+                    {
+                        std::cout << " | Depth16 res: " << depth_img.get_height_pixels() << "x"
+                                  << depth_img.get_width_pixels();
+
+                        cv::Mat frame32;
+                        cv::Mat M = depth_to_opencv(depth_img);
+                        M.convertTo(frame32, CV_32F);
+                        if (n_depth < 1)
+                            mean_depth = frame32.clone();
                         else
-                            mean_color[ch] += frame32[ch].clone();
-                    }
+                            mean_depth += frame32.clone();
 
-                    if (av > 0)
-                    {
-                        std::string tstr = std::to_string(color_img.get_device_timestamp().count());
-                        cv::imwrite(output_dir + "/" + tstr + "-color.png", M);
+                        if (av > 0)
+                        {
+                            std::string tstr = std::to_string(depth_img.get_device_timestamp().count());
+                            cv::imwrite(output_dev_dir + "/" + tstr + "-depth16.png", M);
+                        }
+                        n_depth++;
                     }
-                    n_color++;
                 }
-            }
 
-            if (dump_depth)
-            {
-                // get depth image
-                depth_img = capture.get_depth_image();
-                if (depth_img.is_valid())
+                if (dump_ir)
                 {
-                    std::cout << " | Depth16 res: " << depth_img.get_height_pixels() << "x"
-                              << depth_img.get_width_pixels();
-
-                    cv::Mat frame32;
-                    cv::Mat M = depth_to_opencv(depth_img);
-                    M.convertTo(frame32, CV_32F);
-                    if (n_depth < 1)
-                        mean_depth = frame32.clone();
-                    else
-                        mean_depth += frame32.clone();
-
-                    if (av > 0)
+                    // get ir image
+                    ir_img = capture.get_ir_image();
+                    if (ir_img.is_valid())
                     {
-                        std::string tstr = std::to_string(depth_img.get_device_timestamp().count());
-                        cv::imwrite(output_dir + "/" + tstr + "-depth16.png", M);
+                        std::cout << " | Ir16 res: " << ir_img.get_height_pixels() << "x" << ir_img.get_width_pixels();
+                        cv::Mat frame32;
+                        cv::Mat M = ir_to_opencv(ir_img);
+                        M.convertTo(frame32, CV_32F);
+                        if (n_ir < 1)
+                            mean_ir = frame32.clone();
+                        else
+                            mean_ir += frame32.clone();
+
+                        if (av > 0)
+                        {
+                            std::string tstr = std::to_string(ir_img.get_device_timestamp().count());
+                            cv::imwrite(output_dev_dir + "/" + tstr + "-ir16.png", M);
+                        }
+                        n_ir++;
                     }
-                    n_depth++;
                 }
+                std::cout << std::endl;
+                capture.reset();
             }
 
-            if (dump_ir)
+            if (n_ir > 0 && av != 1)
             {
-                // get ir image
-                ir_img = capture.get_ir_image();
-                if (ir_img.is_valid())
+                mean_ir /= n_ir;
+                cv::Mat ir8, ir16;
+                get_gray_gamma_img(mean_ir, ir8, gray_gamma, gray_max, gray_percentile);
+                mean_ir.convertTo(ir16, CV_16U);
+                cv::imwrite(output_dev_dir + "\\" + "ir8-" + std::to_string(view_idx) + ".png", ir8);
+                cv::imwrite(output_dev_dir + "\\" + "ir16-" + std::to_string(view_idx) + ".png", ir16);
+            }
+            if (n_depth > 0 && av != 1)
+            {
+                mean_depth /= n_depth;
+                cv::Mat depth16;
+                mean_depth.convertTo(depth16, CV_16U);
+                cv::imwrite(output_dev_dir + "\\" + "depth16-" + std::to_string(view_idx) + ".png", depth16);
+            }
+            if (n_color > 1 && av != 1)
+            {
+                cv::Mat channels[3];
+                for (int ch = 0; ch < 3; ch++)
                 {
-                    std::cout << " | Ir16 res: " << ir_img.get_height_pixels() << "x" << ir_img.get_width_pixels();
-                    cv::Mat frame32;
-                    cv::Mat M = ir_to_opencv(ir_img);
-                    M.convertTo(frame32, CV_32F);
-                    if (n_ir < 1)
-                        mean_ir = frame32.clone();
-                    else
-                        mean_ir += frame32.clone();
-
-                    if (av > 0)
-                    {
-                        std::string tstr = std::to_string(ir_img.get_device_timestamp().count());
-                        cv::imwrite(output_dir + "/" + tstr + "-ir16.png", M);
-                    }
-                    n_ir++;
+                    mean_color[ch] /= n_color;
+                    mean_color[ch].convertTo(channels[ch], CV_8U);
                 }
+                cv::Mat color8;
+                cv::merge(channels, 3, color8);
+                cv::imwrite(output_dev_dir + "\\" + "color-" + std::to_string(view_idx) + ".png", color8);
             }
-            std::cout << std::endl;
-            capture.reset();
+            device.stop_cameras();
+            device.close();
         }
 
-        if (n_ir > 0 && av != 1)
-        {
-            mean_ir /= n_ir;
-            cv::Mat ir8, ir16;
-            get_gray_gamma_img(mean_ir, ir8, gray_gamma, gray_max, gray_percentile);
-            mean_ir.convertTo(ir16, CV_16U);
-            cv::imwrite(output_dir + "\\" + "ir8-" + std::to_string(view_idx) + ".png", ir8);
-            cv::imwrite(output_dir + "\\" + "ir16-" + std::to_string(view_idx) + ".png", ir16);
-        }
-        if (n_depth > 0 && av != 1)
-        {
-            mean_depth /= n_depth;
-            cv::Mat depth16;
-            mean_depth.convertTo(depth16, CV_16U);
-            cv::imwrite(output_dir + "\\" + "depth16-" + std::to_string(view_idx) + ".png", depth16);
-        }
-        if (n_color > 1 && av != 1)
-        {
-            cv::Mat channels[3];
-            for (int ch = 0; ch < 3; ch++)
-            {
-                mean_color[ch] /= n_color;
-                mean_color[ch].convertTo(channels[ch], CV_8U);
-            }
-            cv::Mat color8;
-            cv::merge(channels, 3, color8);
-            cv::imwrite(output_dir + "\\" + "color-" + std::to_string(view_idx) + ".png", color8);
-        }
     }
-
-    device.stop_cameras();
-    device.close();
 
     return 0;
 }
